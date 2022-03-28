9a1a1497d5ea48fc2439ad9e97b056a4362e1a9a
ssssujini99 <sssssjin99@naver.com>
2022-03-29 00:32:44 +0900

    Feat(main/views.py): add DetailCocktailView

--- main/views.py
+++ main/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from datetime import datetime
-from .models import Cocktail
-from .serializers import MainSerializer
+from .models import Cocktail, Cocktail_Base, Cocktail_Ingredient
+from .serializers import MainSerializer, DetailSerializer, BaseSerializer, IngredientSerializer
 
 
+# 메인 화면
 class MainCocktailView(APIView):
     def get(self, request):
         today = datetime.today()
@@ -13,3 +14,31 @@ class MainCocktailView(APIView):
         cocktail = get_object_or_404(Cocktail, id=today_cocktail+1)
         serializer = MainSerializer(cocktail)
         return Response(serializer.data)
+
+
+# 상세 페이지
+class DetailCocktailView(APIView):
+    def get(self, request):
+        try:
+            data = {}
+            cocktail = get_object_or_404(Cocktail, id=1) # 테스트로 id=1로 넣음
+            detail = DetailSerializer(cocktail)
+            data["info"] = detail.data
+
+            print(Cocktail_Base.objects.all())
+            bases_querysets = Cocktail_Base.objects.filter(cocktail=cocktail)
+            print(bases_querysets)
+            bases = BaseSerializer(bases_querysets, many=True)
+            print(bases.data)
+            data["bases"] = list(bases.data)
+
+            ingredients_querysets = Cocktail_Ingredient.objects.filter(cocktail=cocktail)
+            print(ingredients_querysets)
+            ingredients = IngredientSerializer(ingredients_querysets, many=True)
+            print(ingredients.data)
+            data['ingredients'] = list(ingredients.data)
+
+            return Response(data)
+
+        except:
+            return Response({"message": "error"})
