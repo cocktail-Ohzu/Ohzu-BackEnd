3b61091cd59c0050a7e5139d86bc9f57a41762dd
ssssujini99 <sssssjin99@naver.com>
2022-03-29 22:45:06 +0900

    Chore(main/views.py): reflect code-reviews

--- main/views.py
+++ main/views.py
@@ -9,27 +9,27 @@ from .serializers import MainSerializer, DetailSerializer, BaseSerializer, Ingre
 # 메인 화면
 class MainCocktailView(APIView):
     def get(self, request):
-        today = datetime.today()
-        today_cocktail = (today.year + today.month + today.day) % 36  # 오늘의 추천 칵테일 랜덤
-        cocktail = get_object_or_404(Cocktail, id=today_cocktail+1)
+        # 최종
+        # today = datetime.today()
+        # today_cocktail = (today.year + today.month + today.day) % 36  # 오늘의 추천 칵테일 랜덤
+        # cocktail = get_object_or_404(Cocktail, id=today_cocktail+1)
+        # 테스트용
+        cocktail = get_object_or_404(Cocktail, id=1)
         serializer = MainSerializer(cocktail)
         return Response(serializer.data)
 
 
 # 상세 페이지
 class DetailCocktailView(APIView):
-    def get(self, request):
+    def get(self, request, id):
         try:
             data = {}
-            cocktail = get_object_or_404(Cocktail, id=1) # 테스트로 id=1로 넣음
+            cocktail = get_object_or_404(Cocktail, id=id)
             detail = DetailSerializer(cocktail)
             data["info"] = detail.data
 
-            print(Cocktail_Base.objects.all())
             bases_querysets = Cocktail_Base.objects.filter(cocktail=cocktail)
-            print(bases_querysets)
             bases = BaseSerializer(bases_querysets, many=True)
-            print(bases.data)
             data["bases"] = list(bases.data)
 
             ingredients_querysets = Cocktail_Ingredient.objects.filter(cocktail=cocktail)
