710b694f9a93f7a135b27dbc37df4434ca5b5890
sukyeong <ssk0967@naver.com>
2022-05-21 04:19:15 +0900

    feat(rec/views): add frame of recommend views
    추천 페이지 틀 잡기

--- main/views.py
+++ main/views.py
@@ -1,18 +1,20 @@
+from collections import defaultdict
+
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from datetime import datetime
-from .models import Cocktail, Cocktail_Base, Cocktail_Ingredient
+from .models import Cocktail, Cocktail_Base, Cocktail_Ingredient, Cocktail_Flavor
 from .serializers import MainSerializer, DetailSerializer, BaseSerializer, IngredientSerializer, SearchSerializer
 
 
 # 메인 화면
 class MainCocktailView(APIView):
     def get(self, request):
-        # today = datetime.today()
-        # today_cocktail = (today.year + today.month + today.day) % 36  # 오늘의 추천 칵테일 랜덤
-        # cocktail = get_object_or_404(Cocktail, id=today_cocktail+1)
-        cocktail = get_object_or_404(Cocktail, id=1)
+        today = datetime.today()
+        today_cocktail = (today.year + today.month + today.day) % 36  # 오늘의 추천 칵테일 랜덤
+        cocktail = get_object_or_404(Cocktail, id=today_cocktail+1)
         serializer = MainSerializer(cocktail)
         return Response(serializer.data)
 
@@ -49,3 +51,39 @@ class SearchTagView(APIView):
             return Response(serializer.data)
         except:
             return Response({"message": "error"})
+
+
+# 추천 페이지
+class RecommendView(APIView):
+    def get(self, request):
+        try:
+            result_cocktail = []
+            data = defaultdict(list)
+
+            id_list = list(request.data)
+
+            for tag_id in id_list:
+                if tag_id == 'base_id':
+                    base_cocktail = []
+                    bases = Cocktail_Base.objects.filter(base_id=request.data['base_id'])
+
+                    for base in bases:
+                        result_cocktail.append(base.cocktail_id)
+                        base_cocktail.append(base.cocktail_id)
+
+                    data['base'] += base_cocktail
+
+                elif tag_id == 'ingredient_id':
+                    ingredient_cocktail = []
+                    ingredients = Cocktail_Ingredient.objects.filter(ingredient_id=request.data['ingredient_id'])
+
+                    for ingredient in ingredients:
+                        result_cocktail.append(ingredient.cocktail)
+                        ingredient_cocktail.append(ingredient.cocktail)
+
+                    data['ingredient'] += ingredient_cocktail
+
+            return Response(data, status=status.HTTP_200_OK)
+
+        except:
+            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
