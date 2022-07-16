1225b238ea597ad7c5872edf1781015a2d181080
ssssujini99 <sssssjin99@naver.com>
2022-07-16 13:59:00 +0900

    Chore(settings/dev.py): add options command

--- Ohzu-BackEnd/settings/dev.py
+++ Ohzu-BackEnd/settings/dev.py
@@ -10,5 +10,8 @@ DATABASES = {
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
         'PORT': env('DATABASE_PORT'),
-    }
+    },
+    'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    },
 }
